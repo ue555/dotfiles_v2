80b681e059e95a04468a903c333561fd1816ea30
Ueki555 <zerozero5320.dev@gmail.com>
2023-10-12 00:12:04 +0900

    プラグインの追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,3 +17,5 @@ end
 bootstrap_pckr()
 
 require('plugins');
+
+require('coc');
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,4 +8,16 @@ require('pckr').add{
   
   -- Gitでファイル編集時に差分を表現する記号が左端に表示される
   'airblade/vim-gitgutter';
+
+  -- tigをvimから扱うプラグイン
+  'iberianpig/tig-explorer.vim';
+  
+  -- Gitのコミットメッセージを表示するプラグイン(コマンドを叩いたときに読み込む)
+  {'rhysd/git-messenger.vim', opt = true, cond = cmd('GitMessenger')};
+  
+  -- coc.nvimのプラグイン
+  {'neoclide/coc.nvim', branch='release'};
+  
+  -- ウィンドウサイズの変更を簡単・高速にするプラグイン
+  'simeji/winresizer';
 }
